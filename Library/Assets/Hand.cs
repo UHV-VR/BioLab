619d8fd77569e52e57f816bce1ed831a7f97ad62
UHV-VR <gamedevuhv@gmail.com>
2019-10-21 09:23:12 -0500

    incubator

---
+++ Library/Assets/Hand.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour
+{
+    //used for picking up and droping the object
+    public SteamVR_Action_Boolean m_GrabAction = null;
+
+    //used to attach to controller 
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private FixedJoint m_joint = null;
+
+    //what our controller is currently holding or interacting with 
+    private Interactable m_CurrentInteractable = null;
+    public List<Interactable> m_ContactInteractable = new List<Interactable>();
+    //change this to private later!!
+
+    public bool triggerDown = false;
+    public string held = "null";
+
+    //needed to fix various bugs 
+    public GameObject otherRemote;
+    public GameObject[] towers;
+    public GameObject[] pieces;
+    private int each = 0;
+    // m = movement 
+    private void Awake()
+    {
+        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+        m_joint = GetComponent<FixedJoint>();
+        towers = GameObject.FindGameObjectsWithTag("bases");
+        pieces = GameObject.FindGameObjectsWithTag("interactable");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //the trigger is being held down 
+        if (m_GrabAction.GetStateDown(m_Pose.inputSource))
+        {
+            // print(m_Pose.inputSource + "Trigger Down");
+            pickup();
+        }
+
+        //up
+        if (m_GrabAction.GetStateUp(m_Pose.inputSource))
+        {
+            // print(m_Pose.inputSource + "Trigger Up");
+            Drop();
+        }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////
+    //collider is the sphere attached to the controller 
+    //adds and removes how many interactables are within range of the remote/ sphere collider 
+    /////////////////////////////////////////////////////////////////////////////////////////
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("interactable"))
+        {
+            return;
+        }
+        m_ContactInteractable.Add(other.gameObject.GetComponent<Interactable>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag("interactable"))
+        {
+            return;
+        }
+        m_ContactInteractable.Remove(other.gameObject.GetComponent<Interactable>());
+
+    }
+    /// <summary>
+    /// adds and removes objects from the remote
+    /// uses a joint to join the remote to the object
+    /// </summary>
+    public void pickup()
+    {
+        if (otherRemote.GetComponent<Hand>().triggerDown)
+        {
+            return;
+        }
+
+        // sets the static to true 
+        triggerDown = true;
+
+
+        //get nearest 
+        m_CurrentInteractable = GetNearestInteractable();
+        //null check 
+        if (!m_CurrentInteractable)
+        {
+            return;
+        }
+        //already held, check
+        if (m_CurrentInteractable.m_ActiveHand)
+        {
+            m_CurrentInteractable.m_ActiveHand.Drop();
+        }
+
+        // ASSIGN THE NAME TO  VARIABLE HELDNAME 
+        held = m_CurrentInteractable.name;
+        m_CurrentInteractable.GetComponent<Rigidbody>().isKinematic = false;
+        // print("assigned heldname with: ");
+        // print(WorldVariables.heldName);
+
+
+        //position to controller 
+        m_CurrentInteractable.transform.position = transform.position;
+
+        //attach
+        Rigidbody targetbody = m_CurrentInteractable.GetComponent<Rigidbody>();
+        m_joint.connectedBody = targetbody;
+
+        //set active hand 
+        m_CurrentInteractable.m_ActiveHand = this;
+    }
+
+    public void Drop()
+    {
+
+        //null check
+        if (!m_CurrentInteractable)
+        {
+            return;
+        }
+
+        //apply velocity
+        Rigidbody targetbody = m_CurrentInteractable.GetComponent<Rigidbody>();
+        targetbody.velocity = m_Pose.GetVelocity();
+        targetbody.angularVelocity = m_Pose.GetAngularVelocity();
+
+
+
+        // TRYING TO RESET THE ROTATION OF THE OBJECT BEFORE IT IS SET dOWN ON A TOWER 
+        targetbody.transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+
+        int interation = 1;
+        //sees that if it wasn't droped from within a range and then resets it to the original tower 
+        foreach (GameObject towerObj in towers)
+        {
+            /*if (!towerObj.GetComponent<tower>().getInRange())
+            {
+                Debug.Log(" tower " + interation + " is not in range");
+                each++;
+            }
+            else
+                Debug.Log(" tower " + interation + " is in in range");
+            interation++;*/
+        }
+        if (each == 3)
+        {
+            Debug.Log("piece is being reset");
+           // targetbody.gameObject.GetComponent<Piece>().reset();
+        }
+        each = 0;
+
+        //detach 
+        m_joint.connectedBody = null;
+
+        //clear 
+        m_CurrentInteractable.m_ActiveHand = null;
+        m_CurrentInteractable = null;
+
+
+        triggerDown = false;
+        held = "null";
+        /* while (true)
+         {
+             each = 0;
+             foreach (GameObject piece in pieces)
+             {
+                 if (!piece.GetComponent<Rigidbody>().IsSleeping())
+                 {
+                     each++;
+                 }
+             }
+             if (each == 3)
+             {
+                 return;
+             }
+             if (each > 3)
+             {
+                 Debug.Log("something went wrong, breaking look");
+                 return;
+
+             }
+
+         }*/
+
+    }
+    /// <summary>
+    ///  want to get the nearest interactable object to the remote 
+    /// </summary>
+    /// <returns></returns>
+
+    private Interactable GetNearestInteractable()
+    {
+        Interactable nearest = null;
+        float minDistance = float.MaxValue;
+        float distance = 0.0f;
+        foreach (Interactable interactable in m_ContactInteractable)
+        {
+            distance = (interactable.transform.position - transform.position).sqrMagnitude;
+
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearest = interactable;
+            }
+        }
+        return nearest;
+    }
+}
